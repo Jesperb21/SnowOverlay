b14d7ea420bf7a062b13a7ed74898b36b928de6d
jesperbaunsgaard <Jesper.j.b.baunsgaard@gmail.com>
2014-12-04 15:00:42 +0100

    first commit

---
+++ TestOverlay/SnowFlake.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace TestOverlay
+{
+    public class SnowFlake
+    {
+        public Ellipse flake;
+        public int x;
+        public int y;
+        public double fallspeed;
+
+        public SnowFlake(float size, int setX, double fallSpeed)
+        {
+            SolidColorBrush brush = new SolidColorBrush(Colors.White);
+            Pen pen = new Pen(brush, size);
+            y = -100;
+            x = setX;
+            flake = new Ellipse();
+            flake.Fill = brush;
+            flake.StrokeThickness = 0.6;
+            flake.Stroke = new SolidColorBrush(Colors.Black);
+            flake.Width = size;
+            flake.Height = size;
+            fallspeed = fallSpeed;
+            
+        }
+    }
+}
